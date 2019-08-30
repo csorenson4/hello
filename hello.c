c33c2f9d8a3b070ce02b8e3b5c4cdf7a608ebae4
Claire <csorenson4@huskers.unl.edu>
2019-08-29 19:09:02 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* Author: Claire Sorenson, Parul Aggarwal, Halle Starns, and Sarah Qudus
+* Date: 2019/08/29
+*
+* A simple hello worls program in C
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Claire Sorenson\n");
+
+  return 0;
+}
